c84cc1a1b05f76e660f38ac85e508aec4a1da47d
Gala <galinas.vm@gmail.com>
2020-11-19 14:46:00 +0200

    inint project

---
+++ App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+declare const global: {HermesInternal: null | {}};
+
+const App = () => {
+  return (
+    <>
+      <SafeAreaView>
+
+      </SafeAreaView>
+    </>
+  );
+};
+
+
+export default App;
